b8d7d787591048d96edb77dce183e00f765ed09a
Pramitha Jayasooriya <lpramithamj@gmail.com>
2024-03-08 21:31:52 +0530

    initializing cashier service

---
+++ src/main/java/com/lifePill/poscashierservice/PosCashierServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lifePill.poscashierservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosCashierServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosCashierServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lifePill/poscashierservice/PosCashierServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lifePill.poscashierservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosCashierServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
